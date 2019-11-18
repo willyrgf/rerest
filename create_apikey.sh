ee70b8383a918a7f330a718751d087421a83d886
Willy Romão <willyr.goncalves@gmail.com>
2019-11-18 12:25:26 +0000

    create a description field on apikey

--- create_apikey.sh
+++ create_apikey.sh
@@ -6,10 +6,13 @@ DB=1
 
 REDIS_CLI=$(command -v redis-cli) || exit 1
 
+[[ -n $1 ]] || exit 1
+desc="$1"
+
 api_key="$(uuidgen -r)" || exit 1
 
 key="api_key:${api_key}"
 
 
 ${REDIS_CLI} -h ${HOST} -p ${PORT} -n ${DB} sadd api_keys:enabled ${key}
-${REDIS_CLI} -h ${HOST} -p ${PORT} -n ${DB} hset ${key} enable true count 0 count_auth 0
+${REDIS_CLI} -h ${HOST} -p ${PORT} -n ${DB} hset ${key} desc "${desc}" enable true count 0 count_auth 0
