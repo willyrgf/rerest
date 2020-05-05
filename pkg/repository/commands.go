ec50736a4748195e746bbf764ee0f854361e9da3
Willy Romão <willyr.goncalves@gmail.com>
2020-05-05 16:17:30 +0000

    add a get from list of redis

--- pkg/repository/commands.go
+++ pkg/repository/commands.go
@@ -11,3 +11,11 @@ func GetFromSet(key string) (members []string, err error) {
 	members, err = redis.Strings(conn.Do("SMEMBERS", key))
 	return
 }
+
+// GetFromList is a layer to get members from a list
+func GetFromList(key string) (members []string, err error) {
+	conn := GetConn()
+	defer conn.Close()
+	members, err = redis.Strings(conn.Do("LRANGE", key, 0, -1))
+	return
+}
